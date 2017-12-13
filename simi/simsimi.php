d45de72e51d3b312b83fbaedf1ce7bcdfe0c8ba2
cajomeshox <34478048+cajomeshox@users.noreply.github.com>
2017-12-13 18:23:15 +0800

    Update simsimi.php

--- simi/simsimi.php
+++ simi/simsimi.php
@@ -4,7 +4,7 @@ if (empty($_GET['pesan'])&&$_GET['pesan']=='') {
 	die('Masukin pesannya bosq');
 }
 $pesan=str_replace(" ", "%20", $_GET['pesan']);
-$key = 'MASUKAN API YANG SUDAH DI DAPATKAN DISINI';
+$key = '9e2f8795-8ea7-4602-b88b-33d6dd528296';
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
